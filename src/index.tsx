54838789cd95a1c8be0ec7c8e38e06298d0a49a5
kurodayuuki <kuroda@attakaido.jp>
2022-05-04 22:11:23 +0900

    不要な記述削除

--- src/index.tsx
+++ src/index.tsx
@@ -1,64 +1,16 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import styled from 'styled-components'
+import { GlobalStyle } from './GlobalStyle'
+import { App } from './App'
 
-function App() {
-  return (
-    <Container>
-      <Header>
-        <Logo>Kanban board</Logo>
-
-        <CardFilter placeholder="Filter cards" />
-      </Header>
-
-      <MainArea>
-        <Column>
-          <ColumnHeader>TODO</ColumnHeader>
-
-          <Card>朝食をとる🍞</Card>
-          <Card>SNSをチェックする🐦</Card>
-          <Card>布団に入る (:3[___]</Card>
-        </Column>
-
-        <Column>
-          <ColumnHeader>Doing</ColumnHeader>
-
-          <Card>顔を洗う👐</Card>
-          <Card>歯を磨く🦷</Card>
-        </Column>
-
-        <Column>
-          <ColumnHeader>Waiting</ColumnHeader>
-        </Column>
-
-        <Column>
-          <ColumnHeader>Done</ColumnHeader>
-
-          <Card>布団から出る (:3っ)っ -=三[＿＿]</Card>
-        </Column>
-      </MainArea>
-    </Container>
-  )
-}
-
-const Container = styled.div``
- 
-const Header = styled.div``
-
-const Logo = styled.div``
-
-const CardFilter = styled.input``
-
-const MainArea = styled.div``
-
-const Column = styled.div``
-
-const ColumnHeader = styled.div``
-
-const Card = styled.div``
 
 const container = document.getElementById('app')
 if (container) {
   const root = createRoot(container)
-  root.render(<App />)
+  root.render(
+    <>
+      <GlobalStyle />
+      <App />
+    </>,
+  )
 }
